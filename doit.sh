24ddba4d9129fa7e13483dd9f977a0865c45052c
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-06-10 12:58:12 -0400

    update config

---
+++ doit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+
+hugo new site . --force
+
+git clone https://github.com/McShelby/hugo-theme-relearn.git themes/relearn
+
+echo 'theme = "relearn"' >> hugo.toml
+
+git clone https://github.com/robinmordasiewicz/fortinet-forticnp-hugo-md.git content/
+
+
+
